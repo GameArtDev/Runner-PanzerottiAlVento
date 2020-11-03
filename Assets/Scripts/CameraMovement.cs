a4c435e9bdd38b753a170067cec324d11ec7aa0f
ema-barr <emanuele.barracchia@hotmail.it>
2020-11-03 19:45:41 +0100

    Initial commit
    Implemented features:
    - Movement
    - Jump and extra jumps
    - Camera following the player

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private bool smoothOn = false;
+    [SerializeField]
+    private float smoothSpeed = 0.7f;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Compute offset on x axis with the player
+        Vector3 moveCamera = new Vector3(player.transform.position.x + offset.x, transform.position.y, transform.position.z);
+
+        if (!smoothOn)
+        {
+            transform.position = moveCamera;
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, moveCamera, smoothSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float speed = 10f;
+    [SerializeField]
+    private float jumpForce = 400f;
+
+    private bool isGrounded;
+    [SerializeField]
+    private Transform groundCheck;
+    [SerializeField]
+    private float checkRadius = 0.5f;
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+
+    [SerializeField]
+    private int maxExtraJumps = 1;
+    private int extraJumps;
+
+    [SerializeField]
+    private bool debugMovement = false;
+    private float moveInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        extraJumps = maxExtraJumps;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        if (debugMovement)
+        {
+            moveInput = Input.GetAxisRaw("Horizontal");
+        } else
+        {
+            moveInput = 1;
+        }
+
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    private void Update()
+    {
+        if (isGrounded)
+        {
+            extraJumps = maxExtraJumps;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                Jump();
+            } else if (extraJumps > 0)
+            {
+                Jump();
+                extraJumps -= 1;
+            }
+        }
+
+
+
+    }
+
+    private void Jump()
+    {
+        rb.velocity = Vector2.up * jumpForce;
+
+    }
+}
